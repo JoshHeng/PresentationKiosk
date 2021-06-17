47b1bd6676432c72723283a92456e7b99c2addd3
Josh Heng <josh@joshheng.co.uk>
2021-06-17 20:47:33 +0100

    Make music fade in/out

--- kiosk/src/Components/musicplayer.js
+++ kiosk/src/Components/musicplayer.js
@@ -1,7 +1,30 @@
 import socket from '../socket';
 
 var cachedTrack, currentTrack;
-var volume = 0.8;
+var volume = 0.1;
+var status = 0; //1 - fade out, 2 = fade in
+
+function fadeIn() {
+	if (!currentTrack || currentTrack.volume >= volume || status !== 2) {
+		if (status === 2) status = 0;
+		return;
+	}
+	
+	currentTrack.volume = Math.min(currentTrack.volume + 0.01, volume);
+	setTimeout(fadeIn, 200);
+}
+function fadeOut() {
+	if (!currentTrack || currentTrack.volume <= 0 || status !== 1) {
+		if (status === 1) {
+			status = 0;
+			if (currentTrack) currentTrack.pause();
+		}
+		return;
+	}
+	
+	currentTrack.volume = Math.max(currentTrack.volume - 0.01, 0);
+	setTimeout(fadeOut, 200);
+}
 
 socket.on('music.load', (url, callback = null) => {
 	cachedTrack = new Audio(url)
@@ -10,6 +33,7 @@ socket.on('music.load', (url, callback = null) => {
 
 socket.on('music.play', (startPaused, callback = null) => {
 	if (currentTrack) currentTrack.pause();
+	status = 0;
 
 	currentTrack = cachedTrack;
 	cachedTrack = null;
@@ -21,11 +45,19 @@ socket.on('music.play', (startPaused, callback = null) => {
 
 	if (callback) callback();
 });
-socket.on('music.pause', () => currentTrack.pause());
-socket.on('music.resume', () => currentTrack.play());
+socket.on('music.pause', () => {
+	status = 1;
+	fadeOut();
+});
+socket.on('music.resume', () => {
+	currentTrack.volume = 0;
+	status = 2;
+	fadeIn();
+	currentTrack.play()
+});
 socket.on('music.volume', (vol, callback = null) => {
-	volume = vol;
-	if (currentTrack) currentTrack.volume = vol;
+	volume = vol/5;
+	if (currentTrack) currentTrack.volume = vol/5;
 
 	if (callback) callback();
 });
